967376e92877a6a596a82d2a95c50a8a0a96d2dc
Ignasi <ignasi@polygon.technology>
2024-10-29 09:27:43 +0100

    Upgrade sp1 verifier to v3 + improve comment at bridgev2

--- docker/scripts/v2/deploy-docker.sh
+++ docker/scripts/v2/deploy-docker.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+# Set the -e option to stop the script if any command fails
+set -e
 sudo rm -rf docker/gethData/geth_data
 sudo DEV_PERIOD=1 docker-compose -f docker/docker-compose.yml up -d geth
 sleep 5
@@ -6,9 +8,11 @@ node docker/scripts/fund-accounts.js
 cp docker/scripts/v2/deploy_parameters_docker.json deployment/v2/deploy_parameters.json
 cp docker/scripts/v2/create_rollup_parameters_docker.json deployment/v2/create_rollup_parameters.json
 npm run deploy:testnet:v2:localhost
+rm -rf docker/deploymentOutput
 mkdir docker/deploymentOutput
 sudo mv deployment/v2/deploy_output.json docker/deploymentOutput
 sudo mv deployment/v2/genesis.json docker/deploymentOutput
+[ -f deployment/v2/genesis_sovereign.json ] && sudo mv deployment/v2/genesis_sovereign.json docker/deploymentOutput
 sudo mv deployment/v2/create_rollup_output.json docker/deploymentOutput
 sudo DEV_PERIOD=1 docker-compose -f docker/docker-compose.yml down
 sudo docker build -t hermeznetwork/geth-zkevm-contracts -f docker/Dockerfile .
